939c7566f4e4a1d32b89bbd76516844a905a13c5
vsantos1711 <vsantos067100@gmail.com>
2024-01-18 20:56:12 -0300

    chorea: start user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from '@/app/user/user.module';
 @Module({
   imports: [
+    UserModule,
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true,
