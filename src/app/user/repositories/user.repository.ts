92d41cd4aa5b4aa1e57e8de4e454ab80dbf99214
vsantos1711 <vsantos067100@gmail.com>
2024-01-18 21:36:36 -0300

    chore: create user repository

---
+++ src/app/user/repositories/user.repository.ts
@@ -0,0 +1,27 @@
+import { UserEntity } from '@/domain/entities/user.entity';
+import { IUserRepository } from '@/domain/repositories/user';
+import { PrismaService } from '@/infra/prisma/prisma.service';
+
+export class UserRepository implements IUserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAll(): Promise<UserEntity[]> {
+    return await this.prisma.user.findMany();
+  }
+  async findById(id: string): Promise<UserEntity> {
+    return await this.prisma.user.findUnique({ where: { id } });
+  }
+  async create(user: UserEntity): Promise<UserEntity> {
+    return await this.prisma.user.create({ data: user });
+  }
+  async update(user: UserEntity): Promise<UserEntity> {
+    return await this.prisma.user.update({
+      where: { id: user.id },
+      data: { ...user },
+    });
+  }
+  async delete(id: string): Promise<boolean> {
+    const deleteUser = await this.prisma.user.delete({ where: { id } });
+    return Boolean(deleteUser);
+  }
+}
