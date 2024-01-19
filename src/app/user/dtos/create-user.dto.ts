22b8ce5d4fa47c45faf0d4527abcc8ace499e5ca
vsantos1711 <vsantos067100@gmail.com>
2024-01-18 21:34:34 -0300

    feat: add create user dto

---
+++ src/app/user/dtos/create-user.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Length(6, 20)
+  password: string;
+}
