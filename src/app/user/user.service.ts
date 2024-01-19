a13ed1bb57e9b920840fd6404831d4d7c005b608
vsantos1711 <vsantos067100@gmail.com>
2024-01-18 21:36:27 -0300

    chore: create user service

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './repositories/user.repository';
+import { UserEntity } from '@/domain/entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async findAll() {
+    return this.userRepository.findAll();
+  }
+  async findById(id: string) {
+    return this.userRepository.findById(id);
+  }
+  async create(user: UserEntity) {
+    return this.userRepository.create(user);
+  }
+  async update(user: UserEntity) {
+    return this.userRepository.update(user);
+  }
+  async delete(id: string) {
+    return this.userRepository.delete(id);
+  }
+}
