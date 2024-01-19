add037e1d682fbcb6eae5ff6bf7a837434f73ed2
vsantos1711 <vsantos067100@gmail.com>
2024-01-18 21:36:19 -0300

    chore: create user controller

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Delete } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get('findAll')
+  findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get('findById/:id')
+  findById(@Param('id') id: string) {
+    return this.userService.findById(id);
+  }
+
+  @Post('create')
+  createUser(@Body() user: CreateUserDto) {
+    return this.userService.create(user);
+  }
+
+  @Post('update')
+  updateUser(@Body() user: UpdateUserDto) {
+    return this.userService.update(user);
+  }
+
+  @Delete('delete/:id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.delete(id);
+  }
+}
