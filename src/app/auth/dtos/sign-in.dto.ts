3219fc6cb53b8391376a92e07a15047954c01fce
vsantos1711 <vsantos067100@gmail.com>
2024-01-22 22:25:26 -0300

    feat: change email to username on SignInDTO

--- src/app/auth/dtos/sign-in.dto.ts
+++ src/app/auth/dtos/sign-in.dto.ts
@@ -3,7 +3,7 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class SignInDTO {
   @IsString()
   @IsNotEmpty()
-  email: string;
+  username: string;
 
   @IsString()
   @IsNotEmpty()
