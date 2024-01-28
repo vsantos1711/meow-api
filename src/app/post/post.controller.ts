985b49c1f484adb8a8cae6f32eadb8566dd832c3
vsantos1711 <vsantos067100@gmail.com>
2024-01-28 17:21:40 -0300

    chore: create Post Controller

--- src/app/post/post.controller.ts
+++ src/app/post/post.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { PostService } from './post.service';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { isPublic } from '../decorators/public.decorator';
+import { RequestInterface } from '../auth/interfaces';
 
-@Controller('user')
+@Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @isPublic()
+  @Get('findAll')
+  async findAll() {
+    return this.postService.findAll();
+  }
+
+  @Get('findByAuthor')
+  async findByAuthor(@Req() req: RequestInterface) {
+    return this.postService.findByAuthor(req.user.sub);
+  }
+
+  @Post('create')
+  async create(@Body() post: CreatePostDto, @Req() req: RequestInterface) {
+    const authorId = req.user.sub;
+    return this.postService.create(post, authorId);
+  }
+
+  @Post('delete/:id')
+  async delete(id: string) {
+    return this.postService.delete(id);
+  }
 }
