c21fbc9923f5efad5c2d90427f1885a594cf9274
vsantos1711 <vsantos067100@gmail.com>
2024-01-28 17:21:51 -0300

    chore: create Post Service

--- src/app/post/post.service.ts
+++ src/app/post/post.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { PostRepository } from './repositories/post.repository';
-
+import { PostEntity } from '@/domain/entities/post.entity';
 @Injectable()
 export class PostService {
   constructor(private readonly postRepository: PostRepository) {}
+
+  async findAll() {
+    return await this.postRepository.listAll();
+  }
+
+  async findByAuthor(id: string) {
+    return await this.postRepository.listByAuthor(id);
+  }
+
+  async create(post: PostEntity, authorId: string) {
+    return await this.postRepository.create(post, authorId);
+  }
+
+  async delete(id: string) {
+    return await this.postRepository.delete(id);
+  }
 }
