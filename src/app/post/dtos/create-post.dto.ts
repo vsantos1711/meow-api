7003042839715d4cb1dda746fef24014a1107f33
vsantos1711 <vsantos067100@gmail.com>
2024-01-28 17:22:07 -0300

    feat: add create post dto

---
+++ src/app/post/dtos/create-post.dto.ts
@@ -0,0 +1,30 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  catName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  catAge: string;
+
+  @IsString()
+  @IsNotEmpty()
+  catBreed: string;
+
+  @IsString()
+  @IsNotEmpty()
+  catWeight: string;
+
+  @IsOptional()
+  views: number;
+
+  @IsOptional()
+  @IsString()
+  authorId: string;
+}
