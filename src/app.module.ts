f25a20c0aa6adaa1a685dda8f7b546011d00faef
vsantos1711 <vsantos067100@gmail.com>
2024-01-31 22:12:57 -0300

    chore: add cron jobs from nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { UserModule } from '@/app/user/user.module';
 import { AuthModule } from '@/app/auth/auth.module';
 import { PostModule } from './app/post/post.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
   imports: [
     UserModule,
     AuthModule,
     PostModule,
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       envFilePath: ['.env'],
       isGlobal: true,
