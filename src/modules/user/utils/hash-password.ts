c146819c945dcc79e107034596fc2cb00ebf88b6
vsantos1711 <vsantos067100@gmail.com>
2024-07-12 17:23:44 -0300

    refactor: decrease salt rounds for password hashing to 8

--- src/modules/user/utils/hash-password.ts
+++ src/modules/user/utils/hash-password.ts
@@ -2,7 +2,7 @@ import { genSalt, hash } from 'bcrypt';
 
 export class HashPassword {
   static async hash(password: string): Promise<string> {
-    const saltOrRounds = 10;
+    const saltOrRounds = 8;
     const salt = await genSalt(saltOrRounds);
     return await hash(password, salt);
   }
